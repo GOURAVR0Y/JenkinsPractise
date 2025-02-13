611cc3033378abb88a2d5b719e4ac5de72fb22d5
Gourav Roy <roy.pushki29@gmail.com>
2025-02-14 00:09:27 +0530

    9th message

--- src/test/java/webUtils/WebClass.java
+++ src/test/java/webUtils/WebClass.java
@@ -25,7 +25,7 @@ public class WebClass {
 		driver.findElement(By.id("submit")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//a[text()='Log out']")).click();
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		driver.quit();
 
 	}
